83467c0ee34e094cfe9cb1c9ab3268ac059fa2a2
cody <cevek@ya.ru>
2015-07-20 11:20:30 +0300

    speed up remove

--- ts/remove.ts
+++ ts/remove.ts
@@ -3,22 +3,25 @@ import {append} from './append';
 import {update} from './update';
 import {normChild} from './utils';
 
-export function remove(node:VNode, parent:VNode, childPos?:number) {
+export function remove(node:VNode, parent:VNode, childPos?:number, skipRemove?:boolean) {
     if (node instanceof VComponent) {
         node.component.componentWillUnmount();
     }
     if (node.children) {
+        var skip = skipRemove || !(node instanceof VFragment);
         for (var i = 0; i < node.children.length; i++) {
-            remove(node.children[i], node, i)
+            remove(node.children[i], node, i, skip);
         }
     }
 
-    if (node instanceof VFragment) {
-        parent.dom.removeChild(node.firstNode);
-        parent.dom.removeChild(node.lastNode);
-    }
-    else {
-        parent.dom.removeChild(node.dom);
+    if (!skipRemove) {
+        if (node instanceof VFragment) {
+            parent.dom.removeChild(node.firstNode);
+            parent.dom.removeChild(node.lastNode);
+        }
+        else {
+            parent.dom.removeChild(node.dom);
+        }
     }
     node.destroy();
     if (childPos != null) {
