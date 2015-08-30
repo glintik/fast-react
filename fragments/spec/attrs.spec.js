0b42f1f5623411c30bf3fbd756b5a388bf36ae2e
cody <cevek@ya.ru>
2015-08-30 15:35:06 +0300

    replace tag when attrs hash changed spec

--- fragments/spec/attrs.spec.js
+++ fragments/spec/attrs.spec.js
@@ -22,31 +22,55 @@ fdescribe("Attrs", () => {
     it('simple', () => {
         new Test()
             .create(<div id={Id1} className={Class1} title={Null} data-name={Null}
-                         onClick={Null} onmousedown={Null}></div>, `<div id="Id1" class="Class1"></div>`, {onclick: null, onmousedown: null})
+                         onClick={Null} onmousedown={Null}></div>, `<div id="Id1" class="Class1"></div>`, {
+                onclick: null,
+                onmousedown: null
+            })
 
             .update(<div id={Id1} className={Class1} title={Title1} data-name={Name1}
-                         onClick={onClick} onmousedown={onMouseDown}></div>, `<div id="Id1" class="Class1" title="Title1" data-name="Name1"></div>`, {onclick: onClick, onmousedown: onMouseDown})
+                         onClick={onClick} onmousedown={onMouseDown}></div>, `<div id="Id1" class="Class1" title="Title1" data-name="Name1"></div>`, {
+                onclick: onClick,
+                onmousedown: onMouseDown
+            })
 
             .update(<div id={Id2} className={Class2} title={Title2} data-name={Name2}
-                         onClick={onClick2} onmousedown={onMouseDown2}></div>, `<div id="Id2" class="Class2" title="Title2" data-name="Name2"></div>`, {onclick: onClick2, onmousedown: onMouseDown2})
+                         onClick={onClick2} onmousedown={onMouseDown2}></div>, `<div id="Id2" class="Class2" title="Title2" data-name="Name2"></div>`, {
+                onclick: onClick2,
+                onmousedown: onMouseDown2
+            })
 
             .update(<div id={Id2} className={Class2} title={Null} data-name={Null}
-                         onClick={Null} onmousedown={Null}></div>, `<div id="Id2" class="Class2"></div>`, {onclick: null, onmousedown: null})
+                         onClick={Null} onmousedown={Null}></div>, `<div id="Id2" class="Class2"></div>`, {
+                onclick: null,
+                onmousedown: null
+            })
     });
 
     it('simple with const', () => {
         new Test()
             .create(<div title={Null} id="Id1" className="Class1" data-name={Null}
-                         onClick={Null} onmousedown={Null}></div>, `<div id="Id1" class="Class1"></div>`, {onclick: null, onmousedown: null})
+                         onClick={Null} onmousedown={Null}></div>, `<div id="Id1" class="Class1"></div>`, {
+                onclick: null,
+                onmousedown: null
+            })
 
             .update(<div title={Title1} id="Id1" className="Class1" data-name={Name1}
-                         onClick={onClick} onmousedown={onMouseDown}></div>, `<div id="Id1" class="Class1" title="Title1" data-name="Name1"></div>`, {onclick: onClick, onmousedown: onMouseDown})
+                         onClick={onClick} onmousedown={onMouseDown}></div>, `<div id="Id1" class="Class1" title="Title1" data-name="Name1"></div>`, {
+                onclick: onClick,
+                onmousedown: onMouseDown
+            })
 
             .update(<div title={Title2} id="Id1" className="Class1" data-name={Name2}
-                         onClick={onClick2} onmousedown={onMouseDown2}></div>, `<div id="Id1" class="Class1" title="Title2" data-name="Name2"></div>`, {onclick: onClick2, onmousedown: onMouseDown2})
+                         onClick={onClick2} onmousedown={onMouseDown2}></div>, `<div id="Id1" class="Class1" title="Title2" data-name="Name2"></div>`, {
+                onclick: onClick2,
+                onmousedown: onMouseDown2
+            })
 
             .update(<div title={Null} id="Id1" className="Class1" data-name={Null}
-                         onClick={Null} onmousedown={Null}></div>, `<div id="Id1" class="Class1"></div>`, {onclick: null, onmousedown: null})
+                         onClick={Null} onmousedown={Null}></div>, `<div id="Id1" class="Class1"></div>`, {
+                onclick: null,
+                onmousedown: null
+            })
     });
 
     it('spread', () => {
@@ -65,22 +89,53 @@ fdescribe("Attrs", () => {
             `<div title="Title1" id="Id1" class="Class1"></div>`, {onclick: onClick, onmousedown: onMouseDown})
 
             .update(<div title={Title1} {...spread4} id={Id1}></div>,
-            `<div title="Title2" id="Id1" class="Class2" rel="Rel2" data-name="Name2"></div>`, {onclick: null, onmousedown: null})
+            `<div title="Title2" id="Id1" class="Class2" rel="Rel2" data-name="Name2"></div>`, {
+                onclick: null,
+                onmousedown: null
+            })
     });
 
-/*
-    it('update const', () => {
+    it('spread to const', () => {
+        var spread1 = {id: Id1, title: Title2, rel: Rel1, "data-name": Name1};
         new Test()
-            .create(<div id="Id" className="Class" title="Title" data-name="Name"></div>, `<div id="Id" class="Class" title="Title" data-name="Name"></div>`)
-            .update(<div id="Id2" className="Class2" title="Title2" data-name="Name2"></div>, `<div id="Id2" class="Class2" title="Title2" data-name="Name2"></div>`)
+            .create(<div title={Title1} {...spread1} id={Id1}></div>,
+            `<div title="Title2" id="Id1" rel="Rel1" data-name="Name1"></div>`)
+
+            .update(<div title={Title1} id={Id1}></div>,
+            `<div title="Title1" id="Id1"></div>`)
+    });
+    it('const to spread', () => {
+        var spread1 = {id: Id1, title: Title2, rel: Rel1, "data-name": Name1};
+        new Test()
+            .create(<div title={Title1} id={Id1}></div>,
+            `<div title="Title1" id="Id1"></div>`)
+
+            .update(<div title={Title1} {...spread1} id={Id1}></div>,
+            `<div title="Title2" id="Id1" rel="Rel1" data-name="Name1"></div>`)
+    });
+    it('const to const', () => {
+        new Test()
+            .create(<div title={Title1} id={Id1}></div>,
+            `<div title="Title1" id="Id1"></div>`)
+
+            .update(<div title={Title2} id={Id1} data-name="Name1"></div>,
+            `<div data-name="Name1" title="Title2" id="Id1"></div>`)
     });
 
-    it('update const', () => {
-        new Test()
-            .create(<div id="Id" className="Class" title="Title" data-name="Name"></div>, `<div id="Id" class="Class" title="Title" data-name="Name"></div>`)
-            .update(<div id="Id2" className="Class2" title="Title2" data-name="Name2"></div>, `<div id="Id2" class="Class2" title="Title2" data-name="Name2"></div>`)
-    });
-*/
+
+    /*
+        it('update const', () => {
+            new Test()
+                .create(<div id="Id" className="Class" title="Title" data-name="Name"></div>, `<div id="Id" class="Class" title="Title" data-name="Name"></div>`)
+                .update(<div id="Id2" className="Class2" title="Title2" data-name="Name2"></div>, `<div id="Id2" class="Class2" title="Title2" data-name="Name2"></div>`)
+        });
+
+        it('update const', () => {
+            new Test()
+                .create(<div id="Id" className="Class" title="Title" data-name="Name"></div>, `<div id="Id" class="Class" title="Title" data-name="Name"></div>`)
+                .update(<div id="Id2" className="Class2" title="Title2" data-name="Name2"></div>, `<div id="Id2" class="Class2" title="Title2" data-name="Name2"></div>`)
+        });
+    */
 
 
 });
