37b9dfb86efe0216586425385f4541d4e456cf2c
Arthur <arthur@neotech.ee>
2015-09-03 14:10:59 +0300

    moving spreads spec, check mount nodes

--- fragments/spec/helper.js
+++ fragments/spec/helper.js
@@ -13,6 +13,33 @@ export class Test {
         return this;
     }
 
+    checkMount(expectMountNodes, realMountNodes){
+        for (var i = 0; i < expectMountNodes.length; i++) {
+            var eNode = expectMountNodes[i];
+            var found = realMountNodes.indexOf(eNode) > -1;
+            if (!found){
+                expect(null).toBe(eNode);
+            }
+        }
+        for (var i = 0; i < realMountNodes.length; i++) {
+            var eNode = realMountNodes[i];
+            var found = expectMountNodes.indexOf(eNode) > -1;
+            if (!found){
+                expect(eNode).toBe(null);
+            }
+        }
+        //expect(expectMountNodes).toEqual(realMountNodes);
+        expectMountNodes.length = 0;
+        realMountNodes.length = 0;
+        return this;
+    }
+
+    clearMount(expectMountNodes, realMountNodes){
+        expectMountNodes.length = 0;
+        realMountNodes.length = 0;
+        return this;
+    }
+
     toBe(html, props) {
         var realHTML = this.div.innerHTML.replace(/\s+/, ' ');
         var expectedHTML = html.replace(/\s+/, ' ');
--- fragments/spec/movings.spec.js
+++ fragments/spec/movings.spec.js
@@ -1,30 +1,41 @@
-import {Test, Component} from './helper';
+import {Test, findDOMNode, Component} from './helper';
 
-class Comp extends Component{
-    render(){
-        return <div>{this.props.value}</div>;
+var realMountNodes = [];
+var expectMountNodes = [];
+
+class Comp extends Component {
+    render() {
+        return <div ref={onMount}>{this.props.value}</div>;
     }
 }
 
-function dk(value) {
-    return <div key={value}>{value}</div>;
+function RD(value) {
+    expectMountNodes.push(`<div>${value}</div>`);
+    return `<div>${value}</div>`;
 }
+
+function onMount(vdom) {
+    realMountNodes.push(findDOMNode(vdom).outerHTML);
+}
+
 function d(value) {
-    return <div>{value}</div>;
+    return <div ref={onMount}>{value}</div>;
 }
-function ck(value) {
-    return <Comp key={value} value={value}></Comp>;
+function dk(value) {
+    return <div ref={onMount} key={value}>{value}</div>;
 }
+
 function c(value) {
     return <Comp value={value}></Comp>;
+}
+
+function ck(value) {
+    return <Comp key={value} value={value}></Comp>;
 }
 
 function rd(value) {
     return `<div>${value}</div>`;
 }
-
-
-
 
 
 class Top extends Component {
@@ -36,7 +47,7 @@ class Top extends Component {
 
 describe("Movings", () => {
     it('div with key and without', () => {
-        new Test()
+        new Test().clearMount(expectMountNodes, realMountNodes)
             .create(<Top values={[dk(0), dk(1), dk(2), dk(3), dk(4), dk(5)]}/>,
             rd([rd(0), rd(1), rd(2), rd(3), rd(4), rd(5)].join('')))
 
@@ -54,7 +65,7 @@ describe("Movings", () => {
     });
 
     it('components with key and without', () => {
-        new Test()
+        new Test().clearMount(expectMountNodes, realMountNodes)
             .create(<Top values={[ck(0), ck(1), ck(2), ck(3), ck(4), ck(5)]}/>,
             rd([rd(0), rd(1), rd(2), rd(3), rd(4), rd(5)].join('')))
 
@@ -72,22 +83,58 @@ describe("Movings", () => {
     });
 
     it('mix', () => {
-        new Test()
+        new Test().clearMount(expectMountNodes, realMountNodes)
             .create(<Top values={[ck(0), [ck(1)], ck(2), ck(3), dk(4), d(5)]}/>,
-            rd([rd(0), rd(1), rd(2), rd(3), rd(4), rd(5)].join('')))
+            rd([RD(0), RD(1), RD(2), RD(3), RD(4), RD(5)].join(''))).checkMount(expectMountNodes, realMountNodes)
 
             .update(<Top values={[c(0), [dk(1), ck(2)], [], dk(6), c(3), dk(4), ck(5)]}/>,
-            rd([rd(0), rd(1), rd(2), rd(6), rd(3), rd(4), rd(5)].join('')))
+            rd([RD(0), RD(1), RD(2), RD(6), RD(3), rd(4), RD(5)].join(''))).checkMount(expectMountNodes, realMountNodes)
 
             .update(<Top values={[ck(5), [c(7), d(4), []], ck(6), ck(2), [], d(1), dk(0)]}/>,
-            rd([rd(5), rd(7), rd(4), rd(6), rd(2), rd(1), rd(0)].join('')))
+            rd([rd(5), RD(7), RD(4), RD(6), RD(2), RD(1), RD(0)].join(''))).checkMount(expectMountNodes, realMountNodes)
 
             .update(<Top values={[[], dk(0), [ck(1), []], d(2), dk(3), dk(4), c(7), ck(5)]}/>,
-            rd([rd(0), rd(1), rd(2), rd(3), rd(4), rd(7), rd(5)].join('')))
+            rd([rd(0), RD(1), RD(2), RD(3), RD(4), RD(7), rd(5)].join(''))).checkMount(expectMountNodes, realMountNodes)
 
             .update(<Top values={[d(7), dk(3), ck(2), dk(4), d(0), ck(1), dk(5), c(6)]}/>,
-            rd([rd(7), rd(3), rd(2), rd(4), rd(0), rd(1), rd(5), rd(6)].join('')))
+            rd([RD(7), rd(3), RD(2), rd(4), RD(0), RD(1), RD(5), RD(6)].join(''))).checkMount(expectMountNodes, realMountNodes)
     });
 
-    //todo movings with spread
+    it('spread mix', () => {
+        function dk(value) {
+            var props = {key: value, ref: onMount};
+            return <div {...props} ref={onMount}>{value}</div>;
+        }
+
+        function d(value) {
+            var props = {ref: onMount};
+            return <div {...props} ref={onMount}>{value}</div>;
+        }
+
+        function ck(value) {
+            var props = {key: value};
+            return <Comp {...props} value={value}></Comp>;
+        }
+
+        function c(value) {
+            var props = {};
+            return <Comp {...props} value={value}></Comp>;
+        }
+
+        new Test().clearMount(expectMountNodes, realMountNodes)
+            .create(<Top values={[ck(0), [ck(1)], ck(2), ck(3), dk(4), d(5)]}/>,
+            rd([RD(0), RD(1), RD(2), RD(3), RD(4), RD(5)].join(''))).checkMount(expectMountNodes, realMountNodes)
+            .update(<Top values={[c(0), [dk(1), ck(2)], [], dk(6), c(3), dk(4), ck(5)]}/>,
+            rd([RD(0), RD(1), RD(2), RD(6), RD(3), rd(4), RD(5)].join(''))).checkMount(expectMountNodes, realMountNodes)
+
+            .update(<Top values={[ck(5), [c(7), d(4), []], ck(6), ck(2), [], d(1), dk(0)]}/>,
+            rd([rd(5), RD(7), RD(4), RD(6), RD(2), RD(1), RD(0)].join(''))).checkMount(expectMountNodes, realMountNodes)
+
+            .update(<Top values={[[], dk(0), [ck(1), []], d(2), dk(3), dk(4), c(7), ck(5)]}/>,
+            rd([rd(0), RD(1), RD(2), RD(3), RD(4), RD(7), rd(5)].join(''))).checkMount(expectMountNodes, realMountNodes)
+
+            .update(<Top values={[d(7), dk(3), ck(2), dk(4), d(0), ck(1), dk(5), c(6)]}/>,
+            rd([RD(7), rd(3), RD(2), rd(4), RD(0), RD(1), RD(5), RD(6)].join(''))).checkMount(expectMountNodes, realMountNodes)
+
+    });
 });
