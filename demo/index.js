45c611e4618d795b7017aa93bb71232eb8963980
Arthur <arthur@neotech.ee>
2015-10-30 11:39:36 +0300

    added demo app

---
+++ demo/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React from 'react';
+
+const abc = <div>hello</div>;
+
+React.render(abc, document.body);
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -28,5 +28,5 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
-    devtool: 'source-map',
+    devtool: 'source-map'
 };
