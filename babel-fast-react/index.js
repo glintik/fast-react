d79f66cc135b54a89acd5921fcc2d19dae4f272c
Arthur <arthur@neotech.ee>
2016-02-08 17:40:07 +0300

    BFR: strange missed code

--- babel-fast-react/index.js
+++ babel-fast-react/index.js
@@ -23,8 +23,8 @@ function fastReact(obj) {
         var key = t.literal(null);
         var ref = t.literal(null);
         var isComponent = t.isIdentifier(tag) || t.isMemberExpression(tag);
-        var base = '\u2425';
-        var VT = {COMPONENT: base + 'C', TAG: base + 'T', TEXT: base + '#'}
+        var base = '␥';
+        var VT = { COMPONENT: base + 'C', TAG: base + 'T', TEXT: base + '#', COMPONENT_CHILDREN: base + 'F' };
 
         array.push(t.literal(isComponent ? VT.COMPONENT : VT.TAG));
         //node
@@ -40,76 +40,102 @@ function fastReact(obj) {
             children.push(child);
         }
 
-
+        var isSpread = false;
         if (isComponent) {
+            if (children.length > 0) {
+                children.unshift(t.thisExpression());
+                //parentNode
+                children.unshift(t.literal(null));
+                children.unshift(t.literal(VT.COMPONENT_CHILDREN));
+                var childrenExpr = t.arrayExpression(children);
+            } else {
+                childrenExpr = t.arrayExpression([t.literal(VT.TEXT), t.literal(null), t.literal('')]);
+            }
             // instance
             array.push(t.literal(null));
-            var isObject = !t.isCallExpression(attrs) && !t.isIdentifier(attrs);
+            // children
+            array.push(t.literal(null));
+
+            if (t.isLiteral(attrs)) {
+                attrs = t.objectExpression([]);
+            }
+            var isObject = t.isObjectExpression(attrs);
             if (isObject) {
-                if (t.isObjectExpression(attrs)) {
-                    var _attrs = attrs.properties;
-                    for (var i = 0; i < _attrs.length; i++) {
-                        var keyNode = _attrs[i].key;
-                        var valueNode = _attrs[i].value;
-                        if (keyNode.name == 'key') {
-                            key = valueNode;
-                            _attrs.splice(i, 1);
-                            i--;
-                        }
-                        if (keyNode.name == 'ref') {
-                            ref = valueNode;
-                            _attrs.splice(i, 1);
-                            i--;
-                        }
-                    }
-                }
-                else {
-                    attrs = t.objectExpression([]);
-                }
-                attrs.properties.push(t.Property('init', t.Identifier('children'), t.arrayExpression(children)));
-            }
-            array.push(attrs);
-
-            if (!isObject) {
-                array.push(t.arrayExpression(children));
-            }
-
-            array.splice(3, 0, key);
-            array.splice(4, 0, ref);
-        }
-        else {
-            var constAttrs = [];
-            var varAttrs = [];
-            var hash = [];
-            //spread
-            if (t.isCallExpression(attrs)) {
-                varAttrs.push(t.literal(null));
-                varAttrs.push(attrs);
-                hash.push("&");
-            }
-            if (t.isObjectExpression(attrs)) {
                 var _attrs = attrs.properties;
                 for (var i = 0; i < _attrs.length; i++) {
                     var keyNode = _attrs[i].key;
                     var valueNode = _attrs[i].value;
                     if (keyNode.name == 'key') {
                         key = valueNode;
-                        continue;
+                        _attrs.splice(i, 1);
+                        i--;
                     }
-
-                    keyNode = t.isLiteral(keyNode) ? keyNode : t.literal(keyNode.name);
-
-                    if (t.isLiteral(valueNode)) {
-                        constAttrs.push(keyNode);
-                        constAttrs.push(valueNode);
-                        hash.push(keyNode.value);
-                    }
-                    else {
-                        varAttrs.push(keyNode);
-                        varAttrs.push(valueNode);
-                        hash.push('&' + keyNode.value);
+                    if (keyNode.name == 'ref') {
+                        ref = valueNode;
+                        _attrs.splice(i, 1);
+                        i--;
                     }
                 }
+                attrs.properties.push(t.Property('init', t.Identifier('children'), childrenExpr));
+            }
+            array.push(attrs);
+
+            if (!isObject) {
+                array.push(childrenExpr);
+            }
+
+            array.splice(3, 0, key);
+            array.splice(4, 0, ref);
+        } else {
+            var constAttrs = [];
+            var varAttrs = [];
+            var hash = [];
+            if (t.isObjectExpression(attrs)) {
+                var _attrs = attrs.properties;
+                for (var i = 0; i < _attrs.length; i++) {
+                    if (_attrs[i].key.name == 'ref') {
+                        isSpread = true;
+                        break;
+                    }
+                }
+                if (!isSpread) {
+                    for (var i = 0; i < _attrs.length; i++) {
+                        var keyNode = _attrs[i].key;
+                        var valueNode = _attrs[i].value;
+                        if (keyNode.name == 'key') {
+                            key = valueNode;
+                            continue;
+                        }
+                        keyNode = t.isLiteral(keyNode) ? keyNode : t.literal(keyNode.name);
+
+                        if (t.isLiteral(valueNode)) {
+                            constAttrs.push(keyNode);
+                            constAttrs.push(valueNode);
+                            hash.push(keyNode.value + '=' + valueNode.value);
+                        } else {
+                            varAttrs.push(keyNode);
+                            varAttrs.push(valueNode);
+                            hash.push('&' + keyNode.value);
+                        }
+                    }
+                }
+            } else {
+                isSpread = true;
+            }
+            if (isSpread && !t.isLiteral(attrs)) {
+                var prop = t.Property('init', t.Identifier('$refComponent'), t.thisExpression());
+                var obj = t.ObjectExpression([prop]);
+                if (t.isObjectExpression(attrs)) {
+                    //attrs.properties.push(prop);
+                } else if (t.isCallExpression(attrs)) {
+                    //attrs.arguments.push(obj);
+                } else if (t.isIdentifier(attrs) || t.isMemberExpression(attrs)) {
+                    //todo
+                    //attrs = attrs;//t.callExpression(t.identifier('_extends'), [t.objectExpression([]), attrs, obj]);
+                }
+                varAttrs.push(t.literal(null));
+                varAttrs.push(attrs);
+                hash.push("~");
             }
             array.push(t.literal(hash.join()));
             array.push(t.literal(varAttrs.length / 2 + constAttrs.length / 2));
